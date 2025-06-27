a6604c626a3eb956ce606091a3f86462f2014f47
Kunal Papnai <kunalpapnai123@gmail.com>
2025-06-27 16:15:58 +0530

    watchlist button and significance of use client

--- src/app/movies/watch/page.jsx
+++ src/app/movies/watch/page.jsx
@@ -1,3 +1,4 @@
+import WishlistButton from "@/components/atom/WishListButton";
 import { buttonVariants } from "@/components/ui/button";
 import { api, ENDPOINT } from "@/lib/api";
 import { FilmIcon } from "lucide-react";
@@ -21,6 +22,7 @@ const page = async ({ searchParams }) => {
                     />
                     <div className="flex flex-wrap gap-4 px-4 lg:px-10 py-8 items-center">
                         <h1 className="text-2xl font-bold">{details.name}</h1>
+                        <WishlistButton />
                     </div>
                 </>
             ) : (
