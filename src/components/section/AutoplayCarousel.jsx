a50039beb301b822d6afa2fbfd65d82f4fba6e0f
Kunal Papnai <kunalpapnai123@gmail.com>
2025-07-01 03:21:15 +0530

    autoplay carousel added

---
+++ src/components/section/AutoplayCarousel.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel';
+import { getWatchUrl, media } from '@/lib/api';
+import Image from 'next/image';
+import Autoplay from "embla-carousel-autoplay"
+import { useRef } from "react"
+import Link from 'next/link';
+
+function AutoplayCarousel({ data }){
+    const plugin = useRef(
+        Autoplay({ 
+            delay: 3000, 
+            stopOnInteraction: true,
+        })
+    )
+
+    return (
+        <Carousel
+            plugins={[plugin.current]}
+            opts={{
+                align: "center",
+                loop: true,
+            }}
+            className="w-full md:px-0 px-4"
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.play}
+        >
+            <CarouselContent className="">
+            {data?.map((vid) => (
+                <CarouselItem key={vid.id} className="w-full max-w-[700px] h-[500px]">
+                    <Link href={getWatchUrl(vid.id, vid.media_type, vid?.poster_path)}>
+                        <Image
+                        src={media(vid?.poster_path)}
+                        alt=""
+                        width={700}
+                        height={500}
+                        className="w-full h-full bg-slate-600 rounded-lg object-fill"
+                        quality={30}
+                        />
+                    </Link>
+                </CarouselItem>
+            ))}
+            </CarouselContent>
+              <div className="absolute bottom-4 right-[12%] hidden md:flex">
+                <CarouselPrevious className="w-[60px] h-[60px] cursor-pointer" />
+                <CarouselNext className="w-[60px] h-[60px] ml-2 cursor-pointer" />
+              </div>
+        </Carousel>
+    )
+}
+
+export default AutoplayCarousel
--- src/components/section/BannerSection.jsx
+++ src/components/section/BannerSection.jsx
@@ -1,10 +1,7 @@
 import React, { Suspense } from 'react'
 import { Skeleton } from '../atom/Skeleton';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel';
-import { getWatchUrl, media } from '@/lib/api';
-import Image from 'next/image';
 import { InboxIcon } from 'lucide-react';
-import Link from 'next/link';
+import AutoplayCarousel from './AutoplayCarousel';
 
 async function BannerSection({fetcher}) {
   return (
@@ -30,34 +27,7 @@ async function BannerSectionContent({fetcher}) {
   }
 
   return (
-    <Carousel
-      opts={{
-        align: "center",
-        loop: true,
-      }}
-      className="w-full  md:px-0 px-4"
-    >
-      <CarouselContent className="">
-        {data?.map((vid) => (
-          <CarouselItem key={vid.id} className="w-full max-w-[700px] h-[500px]">
-            <Link href={getWatchUrl(vid.id, vid.media_type, vid?.poster_path)}>
-              <Image
-                src={media(vid?.poster_path)}
-                alt=""
-                width={700}
-                height={500}
-                className="w-full h-full bg-slate-600 rounded-lg object-fill"
-                quality={30}
-              />
-            </Link>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <div className="absolute bottom-4 right-[12%] hidden md:flex">
-        <CarouselPrevious className="w-[60px] h-[60px] cursor-pointer" />
-        <CarouselNext className="w-[60px] h-[60px] ml-2 cursor-pointer" />
-      </div>
-    </Carousel>
+    <AutoplayCarousel data={data} />
   )
 }
 
